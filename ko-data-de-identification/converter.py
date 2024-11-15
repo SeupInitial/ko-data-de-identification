f58b2eecc5c0c301012d2e8fce85865fc095c6fd
SeupInitial <stevehanhojin@gmail.com>
2024-11-15 16:47:36 +0900

    feat: Add table-to-str-converter

---
+++ ko-data-de-identification/converter.py
@@ -0,0 +1,8 @@
+class ConvertTable:
+    def __init__ (self, data):
+        self.data = data
+        self.strdump = strdump
+    def dumptable(self, data):
+        # 테이블 형태의 데이터를 문자열 덤프로 변환
+        strdump = ""
+        return strdump
